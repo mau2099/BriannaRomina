91b022ac1110e54387abe509ed067c94a7770eb0
Mauricio Garcia <fidel.garcia@globant.com>
2020-04-20 20:21:48 -0500

    Updates

---
+++ precache-manifest.56e8f16013598d0dc763f08a24c8b83e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169914eef1a5fdc459b3715ea1ada810",
+    "url": "/BriannaRomina/index.html"
+  },
+  {
+    "revision": "cab8db74f453ca0fb0e4",
+    "url": "/BriannaRomina/static/css/main.ea62935a.chunk.css"
+  },
+  {
+    "revision": "465a19f03517342e79ac",
+    "url": "/BriannaRomina/static/js/2.69b4c9c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/BriannaRomina/static/js/2.69b4c9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab8db74f453ca0fb0e4",
+    "url": "/BriannaRomina/static/js/main.40f797b6.chunk.js"
+  },
+  {
+    "revision": "cc589a92c684ba192f52",
+    "url": "/BriannaRomina/static/js/runtime-main.c34d6670.js"
+  },
+  {
+    "revision": "fd91372a52a23e6c577c8e29602d6b20",
+    "url": "/BriannaRomina/static/media/Babyground.fd91372a.ttf"
+  },
+  {
+    "revision": "8a0c1ab406fed92fea89de23e2bd9317",
+    "url": "/BriannaRomina/static/media/LittleBaby.8a0c1ab4.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.760142dead8fb8ac28745bf9a783a7a8.js"
+  "/BriannaRomina/precache-manifest.56e8f16013598d0dc763f08a24c8b83e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BriannaRomina/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
