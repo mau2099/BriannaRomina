b12d89f155ca574eb4d022aaa7e963e6edec9223
Mauricio Garcia <fidel.garcia@globant.com>
2020-04-20 20:19:29 -0500

    Updates

---
+++ precache-manifest.760142dead8fb8ac28745bf9a783a7a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b486bc63fe49975e264356b98a9633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "136174104bcd7977065c",
+    "url": "/static/css/main.fdd2777c.chunk.css"
+  },
+  {
+    "revision": "465a19f03517342e79ac",
+    "url": "/static/js/2.69b4c9c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.69b4c9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136174104bcd7977065c",
+    "url": "/static/js/main.6f812647.chunk.js"
+  },
+  {
+    "revision": "85cc4f8c18f793e23872",
+    "url": "/static/js/runtime-main.caabefc2.js"
+  },
+  {
+    "revision": "fd91372a52a23e6c577c8e29602d6b20",
+    "url": "/static/media/Babyground.fd91372a.ttf"
+  },
+  {
+    "revision": "8a0c1ab406fed92fea89de23e2bd9317",
+    "url": "/static/media/LittleBaby.8a0c1ab4.ttf"
+  }
+]);
